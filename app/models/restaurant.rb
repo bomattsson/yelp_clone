c926af54934cf937efd40a95d8cf64f52284452f
Bo Mattsson <bo.mattsson@cint.com>
2015-10-28 11:39:34 +0100

    added model for review average + tests

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,14 @@
 class Restaurant < ActiveRecord::Base
   has_many :reviews, dependent: :destroy
   validates :name, length: {minimum: 3}, uniqueness: {message: "Name already exists"}
+
+  def average_rating
+    return 'N/A' if reviews.none?
+    4
+  end
 end
+
+
 #
 # RSpec::Matchers.define :exist_in_database do
 #   match do /actual/
